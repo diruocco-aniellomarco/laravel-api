6e562cb6c6d60646fff7c221547e2304b3cf087d
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-11-03 19:06:54 +0100

    Route Api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\ProjectController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+// Route::get("/projects", function() {
+//     $projects = Project::paginate(10);
+//     // return response()->json([
+//     //     "projects" => $projects
+//     // ]);
+//     return response()->json($projects);
+// });
+
+Route::apiResource("projects", ProjectController::class)->only('index','show');
